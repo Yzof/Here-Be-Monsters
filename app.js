e8e8ac0557dfef5aa54eee31329ea7681eb5e412
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-18 11:15:52 -0700

    add ? to api key

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let HBMap = new Map;
 HBMap.render();
 
 function loadData() {
-  var url="https://docs.google.com/spreadsheets/d/e/2PACX-1vSZAkr1mDh-DWEIP6kSrERE3Bj4BnGCzCLBVyGJfl_4g0JBWWYSa-YDYGDw9nijqGMFE8JvMkghghPA/pubhtml?gid=1031805306&single=truekey=AIzaSyC3H_vpeG4Vcp2VEx-UjqAIW96US0Ddono";
+  var url="https://docs.google.com/spreadsheets/d/e/2PACX-1vSZAkr1mDh-DWEIP6kSrERE3Bj4BnGCzCLBVyGJfl_4g0JBWWYSa-YDYGDw9nijqGMFE8JvMkghghPA/pubhtml?gid=1031805306&single=true?key=AIzaSyC3H_vpeG4Vcp2VEx-UjqAIW96US0Ddono";
   let xmlhttp = new XMLHttpRequest();
   console.log("base load");
   xmlhttp.open("GET",url,true);
