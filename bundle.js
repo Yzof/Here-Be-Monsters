a9fbbdee892b81846bc22bf75e87ac8220c739dc
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-18 10:43:14 -0700

    Add Testing console logs

--- bundle.js
+++ bundle.js
@@ -78,13 +78,14 @@ HBMap.render();
 function loadData() {
   var url="https://docs.google.com/spreadsheets/d/e/2PACX-1vSZAkr1mDh-DWEIP6kSrERE3Bj4BnGCzCLBVyGJfl_4g0JBWWYSa-YDYGDw9nijqGMFE8JvMkghghPA/pubhtml?gid=1031805306&single=truekey=AIzaSyC3H_vpeG4Vcp2VEx-UjqAIW96US0Ddono";
   let xmlhttp = new XMLHttpRequest();
+  console.log("base load");
   xmlhttp.open("GET",url,true);
-  // xmlhttp.withCredentials = true;
   xmlhttp.onreadystatechange = function() {
     // console.log(xmlhttp.withCredentials);
     // if (xmlhttp.readyState === 4) {
     //   console.log(xmlhttp.status, "ready 4");
     // }
+    console.log("ready state");
     if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
       console.log(xmlhttp.responseText);
       console.log("hello");
