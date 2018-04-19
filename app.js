f9c0890a84f137c74ede9198d24d18fce49ad113
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-19 10:52:15 -0700

    Testi Batch Get

--- app.js
+++ app.js
@@ -10,28 +10,26 @@ function start() {
   gapi.client.init({
     'apiKey': 'AIzaSyC3H_vpeG4Vcp2VEx-UjqAIW96US0Ddono',
     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(function() {
+    gapi.client.sheets.spreadsheets.values.batchGet({
+      spreadsheetId: '10i952_UlOAfVAIBbAVskHEIXqR3miTTdRbQE6xElqzg',
+      range: 'A1:N2',
+      includeGridData: true
+    }).then(function(response) {
+      var range = response.result;
+      if (range.values.length > 0) {
+        console.log(range);
+      } else {
+        // appendPre('No data found.');
+        console.log("No Data Found.");
+      }
+    }, function(response) {
+      // appendPre('Error: ' + response.result.error.message);
+      console.log(response);
+    });
   });
 }
 
-function fetch(id) {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '10i952_UlOAfVAIBbAVskHEIXqR3miTTdRbQE6xElqzg',
-    range: `A${id}:N${id}`,
-    includeGridData: true
-  }).then(function(response) {
-    var range = response.result;
-    if (range.values.length > 0) {
-      console.log(range);
-    } else {
-      // appendPre('No data found.');
-      console.log("No Data Found.");
-    }
-  }, function(response) {
-    // appendPre('Error: ' + response.result.error.message);
-    console.log(response);
-  });
-}
 
 // Loads the JavaScript client library and invokes `start` afterwards.
 gapi.load('client', start);
-gapi.load('client', fetch(2));
