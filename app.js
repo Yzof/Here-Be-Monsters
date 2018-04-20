12d79ccf4c33090ccdc360460d3dcb235df13ce5
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-19 21:35:38 -0700

    header text

--- app.js
+++ app.js
@@ -68,12 +68,17 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get the header tag
+var head = document.getElementById("header");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+    let node = document.createTextNode("words words");
+    head.appendChild(node);
 };
 
 // When the user clicks on <span> (x), close the modal
