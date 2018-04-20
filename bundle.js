421aaa520ca99fa60fe93ce5a43eb438d36acaed
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-19 21:24:10 -0700

    Add modal and styling

--- bundle.js
+++ bundle.js
@@ -117,10 +117,6 @@ function start() {
           let row = extractNum(data.range);
 
           let monster = new Monster(data.values[0], row);
-          //sometimes v is undefined.
-          if (HBMap.biomes[monster.biome] === undefined) {
-            debugger;
-          }
           HBMap.biomes[monster.biome].addMonster(monster);
         }
         console.log(range);
@@ -136,6 +132,31 @@ function extractNum(string) {
   return parseInt(array[array.length - 1]);
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 // Loads the JavaScript client library and invokes `start` afterwards.
 gapi.load('client', start);
