8d79dca56cfa5dca745be82b73838ea48c71b7f4
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-19 22:24:28 -0700

    Detes

--- canvas_visuals/canvas.js
+++ canvas_visuals/canvas.js
@@ -65,39 +65,69 @@ export class Map {
       if (hex) {
         //hex is correct, the biomes aren't being added.
         let detes = this.biomes[hex.biome].details();
-        console.log(detes);
+        this.addDetails(detes);
         // Get the modal
         var modal = document.getElementById('myModal');
-
-        // Get the header tag
-        var head = document.getElementById("header");
 
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
 
         // When the user clicks the button, open the modal
-            modal.style.display = "block";
-            let para = document.createElement("p");
-            let node = document.createTextNode(detes.name);
-            para.appendChild(node);
-            head.appendChild(para);
+          modal.style.display = "block";
 
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
             modal.style.display = "none";
-            para = head.firstElementChild;
-            head.removeChild(para);
+            this.resetDetails();
         };
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
-                para = head.firstElementChild;
-                head.removeChild(para);
+                this.resetDetails();
             }
         };
       }
+    }
+  }
+
+  addDetails(details) {
+    let modalHeader = document.getElementById("modal-header");
+    let head = document.getElementById("header");
+    let para = document.createElement("p");
+    let node = document.createTextNode(details.name);
+
+    //Creates Header text
+    para.appendChild(node);
+    head.appendChild(para);
+    modalHeader.style.backgroundColor = details.color;
+
+    for (var j = 1; j < details.monsters.length + 1; j++) {
+      let mon = details.monsters[j];
+      let li = document.getElementById(`monster${j}`);
+      let name = li.createTextNode(mon.name);
+      li.appendChild(name);
+    }
+  }
+
+  resetDetails() {
+    var head = document.getElementById("header");
+    let div = document.getElementsByClassName("modal-body")[0];
+
+    let ul = div.firstElementChild;
+    let para = head.firstElementChild;
+
+    head.removeChild(para);
+    div.removeChild(ul);
+
+    let newUl = document.createElement('ul');
+    newUl.id = "monster-list";
+
+    for (var i = 1; i < 4; i++) {
+      let newLi = document.createElement('li');
+      newLi.id = `monster${i}`;
+      newUl.appendChild(newLi);
     }
   }
 
