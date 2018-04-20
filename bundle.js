9ca7412f4657c43d7195a580cf62b2d6217bd553
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-19 17:11:09 -0700

    Batch Get

--- bundle.js
+++ bundle.js
@@ -83,7 +83,31 @@ function start() {
   }).then(function() {
     gapi.client.sheets.spreadsheets.values.batchGet({
       spreadsheetId: '10i952_UlOAfVAIBbAVskHEIXqR3miTTdRbQE6xElqzg',
-      ranges: ['A1:N2', 'A3:N3'],
+      ranges: [
+        //Forest
+        'A2:N2', //Awakened Shrub
+        'A170:N170', //Gnoll
+        'A47:N47', //Kobold
+        'A126:N126', //Sprite
+        'A169:N169', //Giant Wasp
+        'A193:N193', //Satyr
+        //Desert
+        'A173:N173', //Hobgoblin
+        'A161:N161', //Dust Mephit
+        'A175:N175', //Jackalwere
+        'A197:N197', //Swarm of Insects
+        'A275:N275', //Thri-Kreen
+        'A599:N599', //Greater Basilisk
+        //Mountain
+        'A185:N185', //Orc
+        'A139:N139', //Winged Kobold
+        'A358:N358', //Ogre
+        'A248:N248', //Harpy
+        'A442:N442', //HellHound
+        'A494:N494', //Ash Drake
+        //Volcano
+        'A959:N959', //Red Dragon
+      ],
       includeGridData: true
     }).then(function(response) {
       var range = response.result;
