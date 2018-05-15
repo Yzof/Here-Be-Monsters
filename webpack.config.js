ed17768e31b9b0ae21aa42a4b1f6e6c1e2ad9951
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-15 14:02:21 -0700

    Added Redux and React, along with some components

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
-var path = require("path");
+const path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -8,8 +29,21 @@ module.exports = {
     filename: "./bundle.js"
   },
   devtool: 'source-map',
+  plugins: plugins,
   resolve: {
     extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
   },
   node: {
     process: false,
